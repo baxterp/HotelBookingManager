1a0238c175d87eebbbb100760e30a6d62cc69b00
Baxter Pearson <baxter.pearson@ehsdata.com>
2022-05-06 13:14:47 +0100

    First checkin

---
+++ HotelBookingManager/Classes/BookingManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelBookingManager.Interfaces;
+
+namespace HotelBookingManager.Classes
+{
+    internal class BookingManager : IBookingManager
+    {
+        public void AddBooking(string guest, int room, DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<int> GetAvailableRooms(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsRoomAvailable(int room, DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
